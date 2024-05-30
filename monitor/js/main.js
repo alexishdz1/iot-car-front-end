7459b2bff7ea8735c66a76ba2e87a89aef5d7df9
alexishdz1 <l20201162@pachuca.tecnm.mx>
2024-05-29 18:06:56 -0600

    Update main.js

--- monitor/js/main.js
+++ monitor/js/main.js
@@ -1,22 +1,21 @@
 
+let adelante = document.getElementById("adelante");
+let derecha = document.getElementById("derecha");
+let detener = document.getElementById("detener");
+let atras = document.getElementById("atras");
+let izquierda = document.getElementById("izquierda");
 
-let Respuesta=document.getElementById('Respuesta');
+let mensaje=document.getElementById("mensaje");
 
-function callApiRequest(){
+function callApi(estatus){
 
 // Make a request for a user with a given ID
-    axios.get('http://34.232.69.248/iot-car-control/back-end/apis/getRegistro.php')
+    axios.get('http://34.232.69.248/iot-car-control/back-end/apis/setRegistro.php?valorEstatus='+estatus)
     .then(function (response) {
         // handle success
+        mensaje.innerHTML = "Respuesta: <strong>" + response.data + "<strong>";
         console.log(response);
-        let respuestaServidor = 
-        response.data=='w'?"ADELANTE":
-        response.data=='a'?"IZQUIERDA":
-        response.data=='d'?"DERECHA":
-        "DETENER";  
-
         
-     Respuesta.innerHTML="ultima orden: <strong>"+ respuestaServidor+"</strong>";
     })
     .catch(function (error) {
         // handle error
@@ -26,5 +25,18 @@ function callApiRequest(){
         // always executed
     });
 }
-
-setInterval(callApiRequest, 2000);
+adelante.addEventListener("click", function (){
+    callApi('w')
+});
+derecha.addEventListener("click", function () {
+    callApi('d')
+});
+detener.addEventListener("click", function () {
+    callApi('q')
+});
+atras.addEventListener("click", function () {
+    callApi('s')
+});
+izquierda.addEventListener("click", function () {
+    callApi('a')
+});
