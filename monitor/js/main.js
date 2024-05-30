2809435e2498019d77e688e291e1a143b46c7447
alexishdz1 <l20201162@pachuca.tecnm.mx>
2024-05-29 18:09:43 -0600

    Update main.js

--- monitor/js/main.js
+++ monitor/js/main.js
@@ -1,42 +1,33 @@
 
-let adelante = document.getElementById("adelante");
-let derecha = document.getElementById("derecha");
-let detener = document.getElementById("detener");
-let atras = document.getElementById("atras");
-let izquierda = document.getElementById("izquierda");
+let respuesta = document.getElementById("respuesta");
 
-let mensaje=document.getElementById("mensaje");
+function callApiRequest() {
 
-function callApi(estatus){
+    // Make a request for a user with a given ID
+    axios.get('http://18.118.26.20/iot-car-control/back-end/apis/getRegistro.php')
+        .then(function (response) {
+            // handle success
+            console.log(response);
 
-// Make a request for a user with a given ID
-    axios.get('http://34.232.69.248/iot-car-control/back-end/apis/setRegistro.php?valorEstatus='+estatus)
-    .then(function (response) {
-        // handle success
-        mensaje.innerHTML = "Respuesta: <strong>" + response.data + "<strong>";
-        console.log(response);
-        
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
-    .finally(function () {
-        // always executed
-    });
+            let respuestaServidor = 
+            response.data == 'w' ? "ADELANTE" : 
+            response.data == 's' ? "ATRAS" : 
+            response.data == 'a' ? "IZQUIERDA" : 
+            response.data == 'd' ? "DERECHA" : "DETENER";
+
+
+                respuesta.innerHTML = "Última Órden: <strong>" + respuestaServidor + "<strong>";
+            
+
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });
 }
-adelante.addEventListener("click", function (){
-    callApi('w')
-});
-derecha.addEventListener("click", function () {
-    callApi('d')
-});
-detener.addEventListener("click", function () {
-    callApi('q')
-});
-atras.addEventListener("click", function () {
-    callApi('s')
-});
-izquierda.addEventListener("click", function () {
-    callApi('a')
-});
+
+
+setInterval(callApiRequest, 1000);
